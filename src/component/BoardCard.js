b99ac69ad067aa46c4b6a9d5f2030714c6a1efac
bum752 <bum752@gmail.com>
2020-01-01 22:02:10 +0900

    보드 생성 컴포넌트 마크업 추가

--- src/component/BoardCard.js
+++ src/component/BoardCard.js
@@ -13,7 +13,7 @@ const CardDiv = styled.div`
   flex-direction: column;
   -webkit-box-pack: center;
   align-items: center;
-  board-radius: 5.5px;
+  border-radius: 5.5px;
   box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
   cursor: pointer;
   transition: 200ms ease-in-out;
@@ -22,14 +22,18 @@ const CardDiv = styled.div`
 `;
 
 const NewBoardCardHeaderDiv = styled.div`
-  color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: #fff;
   text-shadow: 0px 0px 3px #000;
   width: 100%;
   border-bottom: solid 1px rgb(240, 240, 240);
+  font-weight: bold;
 `;
 
 const NewBoardCardBodyDiv = styled.div`
-  color: white;
+  color: #fff;
   text-shadow: 0px 0px 3px #000;
   margin: 10px;
 `;
@@ -38,15 +42,63 @@ const FloatRightDiv = styled.div`
   float: right;
 `;
 
+const BoardNameInput = styled.input`
+  padding: 11px 15px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 2.5px solid rgba(255, 255, 255, 0.8);
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.16), 0 3px 3px rgba(0, 0, 0, 0.23);
+`;
+
+const BoardNameErrorDiv = styled.div`
+  color: #000;
+  text-shadow: 1px 1px 1px #fff;
+  font-weight: 400;
+  margin: 10px 0 0 0;
+  text-align: center;
+  font-size: 12px;
+`;
+
+const ButtonsDiv = styled.div`
+  display: flex;
+  width: 150px;
+  justify-content: space-between;
+  margin-top: 15px;
+`;
+
+const CancelButton = styled.button`
+  width: 70px;
+  height: 43px;
+  text-transform: uppercase;
+  cursor: pointer;
+  border: none;
+  background: none;
+  color: #fff;
+  font-weight: 900;
+  text-shadow: 0px 0px 3px #000;
+`;
+
+const CreateButton = styled.button`
+  width: 70px;
+  height: 43px;
+  text-transform: uppercase;
+  cursor: pointer;
+  border: none;
+  background: #fff;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.16), 0 3px 3px rgba(0, 0, 0, 0.23);
+`;
+
 class BoardCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      newBoard: false
+      newBoard: false,
+      newBoardName: ''
     };
 
     this.createNewBoard = this.createNewBoard.bind(this);
     this.cancelNewBoard = this.cancelNewBoard.bind(this);
+    this.handleNewBoardNameInputChange = this.handleNewBoardNameInputChange.bind(this);
   }
 
   createNewBoard() {
@@ -57,23 +109,44 @@ class BoardCard extends React.Component {
     this.setState({ newBoard: false });
   }
 
+  handleNewBoardNameInputChange(event) {
+    this.setState({ newBoardName: event.target.value });
+  }
+
   render() {
     return (
       <>
         {this.state.newBoard ? (
           <CardDiv>
             <NewBoardCardHeaderDiv>
-              Creating a board
+              <h3>Creating a board</h3>
               <FloatRightDiv>
                 <FontAwesomeIcon icon={faTimes} onClick={this.cancelNewBoard} />
               </FloatRightDiv>
             </NewBoardCardHeaderDiv>
             <NewBoardCardBodyDiv>
-              What shall we call the board?
+              <h5>What shall we call the board?</h5>
             </NewBoardCardBodyDiv>
+            <BoardNameInput
+              value={this.state.newBoardName}
+              onChange={this.handleNewBoardNameInputChange}
+            ></BoardNameInput>
+            {!this.state.newBoardName ? (
+              <BoardNameErrorDiv>Oops! Looks like you forgot the name!</BoardNameErrorDiv>
+            ) : (
+              <></>
+            )}
+            <ButtonsDiv>
+              <CancelButton onClick={this.cancelNewBoard}>Cancel</CancelButton>
+              <CreateButton>Create</CreateButton>
+            </ButtonsDiv>
           </CardDiv>
         ) : (
-          <CardDiv onClick={this.createNewBoard}>Create a new board...</CardDiv>
+          <CardDiv onClick={this.createNewBoard}>
+            <NewBoardCardBodyDiv>
+              <h3>Create a new board...</h3>
+            </NewBoardCardBodyDiv>
+          </CardDiv>
         )}
       </>
     );
